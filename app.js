b2161a452809a3d6eca703d5ae5a89a3f5c9096a
shivkanth <shivkanthb@gmail.com>
2015-07-13 21:26:48 -0700

    express server up and running

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var cons = require('consolidate');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 3000;
+
+var mongoose = require('mongoose');
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine('html', cons.swig);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function(req,res){
+	res.render('index.html');
+});
+
+app.listen(port);
+console.log("server running on port"+port);
