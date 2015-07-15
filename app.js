34504aa5f2cae7889f500882871a3e645e07f359
shivkanth <shivkanthb@gmail.com>
2015-07-14 21:42:48 -0700

    basic auth express added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Comment = require('./models/Comment');
 var app = express();
+var http = require('http');
 var path = require('path');
 var cons = require('consolidate');
 var bodyParser = require('body-parser');
@@ -13,10 +14,42 @@ app.use(express.static(__dirname));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.engine('html', cons.swig);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
+
+
+app.use(function(req, res, next) {
+    var auth;
+
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    // checks if:
+    // * auth array exists 
+    // * first value matches the expected user 
+    // * second value the expected password
+    if (!auth || auth[0] !== 'admin' || auth[1] !== 'admin') {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="Hello"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        next();
+    }
+});
+
 
 app.get('/', function(req,res){
 	res.render('index.html');
